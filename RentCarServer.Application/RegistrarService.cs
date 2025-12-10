4f3bdbb9bbfe9789088b28e2355bc9871e770308
onur <onurapple2000@gmail.com>
2025-12-10 12:59:28 +0300

    Application layer initialized

---
+++ RentCarServer.Application/RegistrarService.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using RentCarServer.Application.Behaviors;
+
+namespace RentCarServer.Application;
+
+public static class RegistrarService
+{
+    public static IServiceCollection AddAplication(this IServiceCollection services)
+    {
+        // Add application services here
+
+        services.AddMediatR(cfr =>
+        {
+            cfr.RegisterServicesFromAssembly(typeof(RegistrarService).Assembly);
+            cfr.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfr.AddOpenBehavior(typeof(PermissionBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(typeof(RegistrarService).Assembly);
+
+        return services;
+    }
+}
